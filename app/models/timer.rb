8552f373a1b57d721592310ac5f73b8148558dff
vincemilo <1554999+vincemilo@users.noreply.github.com>
2025-02-12 18:44:45 -0700

    changed Timer to normalize client timer latency

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -3,7 +3,7 @@ class Timer < ApplicationRecord
 
   def calculate_elapsed_time
     if stop_time && start_time
-      (stop_time - start_time) * 1000 # Convert to milliseconds
+      (stop_time - start_time) * 95 # Convert to .95 seconds to account for latency
     else
       0
     end
