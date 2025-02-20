a0e082abc59162fb3a00aeb310bdb755aa09d478
vincemilo <1554999+vincemilo@users.noreply.github.com>
2025-02-20 00:22:12 -0700

    created cleanup method for highscores to get rid of old data, got rid of location options for chars and high_scores controllers as is api only

--- app/controllers/high_scores_controller.rb
+++ app/controllers/high_scores_controller.rb
@@ -9,9 +9,15 @@ class HighScoresController < ApplicationController
     @high_score = HighScore.new(high_score_params)
 
     if @high_score.save
-      render json: @high_score, status: :created, location: @high_score
+      render json: @high_score, status: :created
     else
       render json: @high_score.errors, status: :unprocessable_entity
     end
+  end
+
+  private
+  # Only allow a list of trusted parameters through.
+  def high_score_params
+    params.expect(high_score: [ :name, :time ])
   end
 end
--- app/models/high_score.rb
+++ app/models/high_score.rb
@@ -2,6 +2,8 @@ class HighScore < ApplicationRecord
   before_create :cleanup
 
   def cleanup
-    HighScore.order(:time)
+    # remove all records lower than the top ten
+    top_ten_ids = HighScore.order(:time).limit(9).pluck(:id)
+    HighScore.where.not(id: top_ten_ids).delete_all
   end
 end
