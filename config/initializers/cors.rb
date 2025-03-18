6a3321cc59372269d82092a14ab85777b9f50d55
Vince <1554999+vincemilo@users.noreply.github.com>
2025-03-17 22:38:57 -0600

    tinkering iwth cors and fly token

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins [ "localhost:5173", "https://vincemilowhereswaldo.netlify.app" ]
+      origins "localhost:5173"
 
       resource "*",
         headers: :any,
